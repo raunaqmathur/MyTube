0d1607518a44c42a998d5f8d6ae3a2b66bcd25cb
raunaqmathur <raunaq.mathur22@gmail.com>
2015-10-04 12:49:09 -0700

    Youtube sign in done

---
+++ app/src/main/java/com/company/raunaqmathur/mytube/SearchActivity.java
@@ -0,0 +1,92 @@
+package com.company.raunaqmathur.mytube;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.api.services.youtube.YouTube;
+
+import java.io.IOException;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private YouTube youtube;
+    private YouTube.Search.List query;
+    public static final String KEY= "AIzaSQZZQWQQWMGziK9H_qRxz8g-V6eDL3QW_Us";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void searchClicked(View view)
+    {
+
+
+
+
+
+        youtube = new YouTube.Builder(new NetHttpTransport(),
+                new JacksonFactory(), new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest hr) throws IOException {}
+        }).setApplicationName(content.getString(R.string.app_name)).build();
+
+        try{
+            query = youtube.search().list("id,snippet");
+            query.setKey(KEY);
+            query.setType("video");
+            query.setFields("items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)");
+        }catch(IOException e){
+            Log.d("YC", "Could not initialize: " + e);
+        }
+
+    }
+
+    public List<VideoItem> search(String keywords){
+        query.setQ(keywords);
+        try{
+            SearchListResponse response = query.execute();
+            List<SearchResult> results = response.getItems();
+
+            List<VideoItem> items = new ArrayList<VideoItem>();
+            for(SearchResult result:results){
+                VideoItem item = new VideoItem();
+                item.setTitle(result.getSnippet().getTitle());
+                item.setDescription(result.getSnippet().getDescription());
+                item.setThumbnailURL(result.getSnippet().getThumbnails().getDefault().getUrl());
+                item.setId(result.getId().getVideoId());
+                items.add(item);
+            }
+            return items;
+        }catch(IOException e){
+            Log.d("YC", "Could not search: "+e);
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/company/raunaqmathur/mytube/SearchItem.java
@@ -0,0 +1,44 @@
+package com.company.raunaqmathur.mytube;
+
+/**
+ * Created by raunaqmathur on 10/2/15.
+ */
+public class SearchItem {
+
+    private String title;
+    private String description;
+    private String thumbnailURL;
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnail) {
+        this.thumbnailURL = thumbnail;
+    }
+}
